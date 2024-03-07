d5808d7c631b9e469ed0ab0749215104eb016419
prathap-personal <prathap.chandra.754@gmail.com>
2024-03-07 12:39:35 +0530

    changed the port

--- node-server/server.js
+++ node-server/server.js
@@ -25,7 +25,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const port = 3000;
+const port = 8642;
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
